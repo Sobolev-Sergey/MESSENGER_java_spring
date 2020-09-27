3bce2fa0e4066ff542bca8ab983a75279294db9c
Sergey Sobolev <skspochta@mail.ru>
2020-09-27 18:02:36 +0300

    Add Admin role and check rights by `hasAuthority`

--- src/main/java/com/example/messenger/domain/Role.java
+++ src/main/java/com/example/messenger/domain/Role.java
@@ -3,6 +3,7 @@ package com.example.messenger.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
+    ADMIN,
     USER;
 
     @Override
